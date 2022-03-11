599fc6929cc1d72d1e72c081bd2337dee2578966
shawncocklin <shawncocklin@gmail.com>
2022-03-11 17:05:25 -0500

    added inputs and labels for box shadow widget

--- src/components/BoxShadow.svelte
+++ src/components/BoxShadow.svelte
@@ -1,3 +1,23 @@
 <script></script>
 
 <h1>Box Shadow</h1>
+
+<div class="num-inputs">
+  <label for="xOffset">X Offset</label>
+  <input type="number" name="xOffset" id="xOffset" />
+  <label for="yOffset">Y Offset</label>
+  <input type="number" name="yOffset" id="yOffset" />
+  <label for="blur">Blur</label>
+  <input type="number" name="blur" id="blur" />
+  <label for="spread">Spread</label>
+  <input type="number" name="spread" id="spread" />
+</div>
+
+<div class="clr-inputs">
+  <label for="shadow">Shadow Color</label>
+  <input type="color" name="shadow" id="shadow" />
+  <label for="box">Box Color</label>
+  <input type="color" name="box" id="box" />
+  <label for="background">Background Color</label>
+  <input type="color" name="background" id="background" />
+</div>
